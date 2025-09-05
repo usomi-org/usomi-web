370163eec93955f77a1514e3a4a648341e52d31d
rayjonesjay <rayjaymuiruri@gmail.com>
2025-09-05 14:17:21 +0300

    feat(component): customizable image component

---
+++ components/ImageComponent.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+interface ImageComponentProps {
+    imagePath: string;
+    width: number;
+    height: number;
+    alt?: string; // optional unless overridden
+    className?: string;
+}
+
+const ImageComponent = ({imagePath, width, height, alt = 'Descriptive Image', className = 'rounded-2xl object-cover'}: ImageComponentProps) => {
+    return (
+        <div className="mt-8">
+            <Image
+                src={imagePath}
+                alt={alt}
+                width={width}
+                height={height}
+                className={className}
+            />
+        </div>
+    );
+};
+
+export default ImageComponent;
