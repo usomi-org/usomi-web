d9d7b03bfe145439bd1130f29b05ba3b16289816
rayjonesjay <rayjaymuiruri@gmail.com>
2025-09-09 14:07:55 +0300

    feat(photos): stock photos

---
+++ lib/images.ts
@@ -0,0 +1,23 @@
+export const StockImages = [
+    "/usomi_4.jpg",
+    "/usomi_1.jpg",
+    "/usomi_2.jpg",
+    "/usomi_3.jpg",
+    "/usomi_5.jpg",
+    "/usomi_6.jpg",
+    "/usomi_7.jpg",
+    "/usomi_8.jpg",
+    "/usomi_9.jpg",
+    "/usomi_10.jpg",
+    "/usomi_11.jpg",
+    "/usomi_12.jpg",
+    "/usomi_13.jpg",
+    "/usomi_14.jpg",
+    "/usomi_15.jpg",
+];
+
+export const AboutUsomi: string = `USOMI Limited is a technical services provision company that seeks to address the poor
+ adoption of agricultural technologies and farming best practices. Our goal is to increase access to the right inputs,
+  markets and production maximization knowledge and information. Through our mobile and web platforms, we help farmers 
+  purchase authentic high-quality inputs, provide a mechanism for data-driven bench marking while also ensuring 
+  that farmers can aggregate their produce and sell directly to high value domestic and international markets. We transform lives.`
