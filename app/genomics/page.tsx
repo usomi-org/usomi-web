3639b6ea043936f27e6116017f206025063749d7
rayjonesjay <rayjaymuiruri@gmail.com>
2025-09-14 23:28:47 +0300

    to be developed pages

---
+++ app/genomics/page.tsx
@@ -0,0 +1,5 @@
+export default function Genomics() {
+    return (
+        <h1>genomics page is still in development</h1>
+    )
+}
---
+++ app/lulu/page.tsx
@@ -0,0 +1,5 @@
+export default function Lulu() {
+    return (
+        <h1>Lulu page is still in development</h1>
+    )
+}
